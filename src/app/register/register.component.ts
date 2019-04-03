f8d62d3569ed7338008dee8dcf037d26d061d3e8
nassou78 <jogafni@gmail.com>
2019-04-03 19:43:09 +0200

    #2 Validators

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
-import {FormControl, FormGroup} from '@angular/forms';
+import {FormControl, FormGroup, FormBuilder, Validators} from '@angular/forms';
+import {UserService} from '../../services/user.service';
+import {assertNumber} from '@angular/core/src/render3/assert';
 
 @Component({
   selector: 'app-register',
@@ -8,24 +10,28 @@ import {FormControl, FormGroup} from '@angular/forms';
 })
 export class RegisterComponent implements OnInit {
   sexe = ['Homme', 'Femme', 'Autre'];
-  filiere = ['SI', 'ELEC', 'BAT', 'GE', 'GB' ]
+  filiere = ['SI', 'ELEC', 'BAT', 'GE', 'GB'];
 
-  profilForm = new FormGroup({
-    mail: new FormControl(''),
-    mdp: new FormControl(''),
-    nom: new FormControl(''),
-    prenom: new FormControl(''),
-    birthday: new FormControl(''),
-    sexe: new FormControl(''),
-    nationalite: new FormControl(''),
-    adresse: new FormControl(''),
-    ville: new FormControl(''),
-    codePostal: new FormControl(''),
-    tel: new FormControl(''),
-    numetudiant: new FormControl(''),
-    filiere: new FormControl(''),
+  public profilForm: FormGroup;
 
-  });
+
+  constructor(public formBuilder: FormBuilder, public studentService: UserService) {
+    this.profilForm = this.formBuilder.group({
+      mail: new FormControl('', [Validators.email, Validators.required]),
+      mdp: new FormControl(''),
+      nom: new FormControl(''),
+      prenom: new FormControl(''),
+      birthday: new FormControl(''),
+      sexe: new FormControl(''),
+      nationalite: new FormControl(''),
+      adresse: new FormControl(''),
+      ville: new FormControl(''),
+      codePostal: new FormControl(''),
+      tel: new FormControl(''),
+      numetudiant: new FormControl('',[Validators.pattern('[0-9]*')]),
+      filiere: new FormControl(''),
+    });
+  }
 
 
   ngOnInit() {
