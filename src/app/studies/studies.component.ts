23530c2939359987c415666b2ff9ecd26a4de02a
FazulNazar <43197941+FazulNazar@users.noreply.github.com>
2019-04-09 09:55:06 +0200

    #4 reacitvform

--- src/app/studies/studies.component.ts
+++ src/app/studies/studies.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import {UserService} from '../../services/user.service';
 
 @Component({
   selector: 'app-studies',
@@ -6,8 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./studies.component.css']
 })
 export class StudiesComponent implements OnInit {
+  public  studiesForm: FormGroup;
 
-  constructor() { }
+  constructor(public formBuilder: FormBuilder, public studentService: UserService) {
+
+    this. studiesForm = this.formBuilder.group({
+      mail: new FormControl('', [Validators.email, Validators.required]),
+      school: new FormControl(''),
+      year: new FormControl(''),
+      grade: new FormControl(''),
+      locality: new FormControl(''),
+      ville: new FormControl(''),
+      speciality: new FormControl(''),
+      mean: new FormControl(''),
+    });
+  }
+
+
 
   ngOnInit() {
   }
